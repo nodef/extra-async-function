d2a9b8190bc4d3bf59e0916c9a022bfd5edc16d7
Subhajit Sahu <wolfram77@gmail.com>
2020-12-16 19:23:52 +0530

    :bus: for security issue with ini

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-export {default as ID} from './ID';
-export {default as WAIT} from './WAIT';
 export {default as arity} from './arity';
 export {default as backoff} from './backoff';
+export {default as ID} from './ID';
 export {default as is} from './is';
 export {default as memoize} from './memoize';
 export {default as name} from './name';
 export {default as parameters} from './parameters';
 export {default as signature} from './signature';
+export {default as WAIT} from './WAIT';
